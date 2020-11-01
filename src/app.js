1310f210d77aaf80fdd6b3a2b031e1718c3c8b63
Aleksander <Zakharov.owl@gmail.com>
2020-11-01 16:00:43 +0300

    full swiper

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ let controlRight = document.querySelector('.control-right');
 let currentItem = 0;
 let isEnabled = true; //flag for animation
 
-function changeCurrentItem(n){
+function changeCurrentItem(n) {
     currentItem = (n + items.length) % items.length; //прокрутка, возвращение к lenght[0]
 
 }
@@ -13,57 +13,59 @@ function changeCurrentItem(n){
                 SLIDER
 */
 
-function hideItem(direction){
+function hideItem(direction) {
     /*отключаю стрелку на время действия анимации*/
     isEnabled = false;
     items[currentItem].classList.add(direction);
-    items[currentItem].addEventListener('animationend', function(){
+    items[currentItem].addEventListener('animationend', function () {
         this.classList.remove('item-active', direction)
     })
 }
 
-function showItem(direction){
+function showItem(direction) {
     items[currentItem].classList.add('item-next', direction);
-    items[currentItem].addEventListener('animationend', function(){
+    items[currentItem].addEventListener('animationend', function () {
         this.classList.remove('item-next', direction);
         this.classList.add('item-active');
         isEnabled = true;
     })
 }
 
-function previousItem(n){
-    hideItem('to-right');            
-    changeCurrentItem(n-1);
+function previousItem(n) {
+    hideItem('to-right');
+    changeCurrentItem(n - 1);
     showItem('from-left')
 }
 
-function nextItem(n){
-    hideItem('to-left');           
-    changeCurrentItem(n+1);
+function nextItem(n) {
+    hideItem('to-left');
+    changeCurrentItem(n + 1);
     showItem('from-right')
 }
 
-controlLeft.addEventListener('click', function(){
+controlLeft.addEventListener('click', function () {
     //console.log('hi');
-    if (isEnabled){
+    if (isEnabled) {
         previousItem(currentItem)
     }
 })
 
-controlRight.addEventListener('click', function(){
+controlRight.addEventListener('click', function () {
     //console.log('hi');
-    if (isEnabled){
+    if (isEnabled) {
         nextItem(currentItem)
     }
 })
 
 /*
+
                 SWIPER
+
 */
 let el = document.querySelector('.carousel')
 
 
-const swipeDetect = (el)=>{
+const swipeDetect = (el) => {
 
     let surface = el;
     let startX = 0; // Х
@@ -75,8 +77,94 @@ const swipeDetect = (el)=>{
     let elapsedTime = 0; // finish time to swipe
 
     let threshold = 150; //minimal distance swiper
-    let restraint = 100;  // угол for swipe
-    let allowedTime = 100; // time to swipe
+    let restraint = 120; // угол for swipe
+    let allowedTime = 400; // time swipe
+
+    /*
+            mouse events
+    */
+
+
+    surface.addEventListener('mousedown', function (e) {
+        startX = e.pageX;
+        startY = e.pageY;
+        startTime = new Date().getTime();
+        e.preventDefault();
+    })
+
+    surface.addEventListener('mouseup', function (e) {
+        distX = e.pageX - startX;
+        distY = e.pageY - startY;
+        elapsedTime = new Date().getTime() - startTime;
+
+        if (elapsedTime <= allowedTime) {
+            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
+                if ((distX > 0)) { //проверка на анимацию
+                    if (isEnabled) {
+                        previousItem(currentItem);
+                    }
+                } else {
+                    if (isEnabled) {
+                        nextItem(currentItem);
+                    }
+                }
+            }
+        }
+        e.preventDefault();
+    })
+
+
+    /*
+            touch events
+    */
+
+    surface.addEventListener('touchstart', function (e) {
+        if(e.target.classList.contains('control-arrow') || e.target.classList.contains('control')){
+            if(e.target.classList.contains('control-arrow__left')){
+                if (isEnabled) {
+                    previousItem(currentItem);
+                }
+            } else if(e.target.classList.contains('control-arrow__right')){
+                if (isEnabled) {
+                    nextItem(currentItem);
+                }
+            }
+        }
+    
+
+        let touchObj = e.changedTouches[0];
+        startX = touchObj.pageX;
+        startY = touchObj.pageY;
+        startTime = new Date().getTime();
+        e.preventDefault();
+    })
+
+    //position page static
+    surface.addEventListener('touchmove', function (e) {
+        e.preventDefault();
+    })
+
+    surface.addEventListener('touchend', function (e) {
+        let touchObj = e.changedTouches[0];
+        distX = touchObj.pageX - startX;
+        distY = touchObj.pageY - startY;
+        elapsedTime = new Date().getTime() - startTime;
+
+        if (elapsedTime <= allowedTime) {
+            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
+                if ((distX > 0)) { //проверка на анимацию
+                    if (isEnabled) {
+                        previousItem(currentItem);
+                    }
+                } else {
+                    if (isEnabled) {
+                        nextItem(currentItem);
+                    }
+                }
+            }
+        }
+        e.preventDefault();
+    })
 
 }
 
